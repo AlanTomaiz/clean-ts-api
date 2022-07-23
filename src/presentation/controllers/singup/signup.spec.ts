01d893e3f700f8dcaf8655454a35608da002268c
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2022-07-23 16:57:48 -0300

    test: ensure SignUpController return status 200 if valid data is provided

--- src/presentation/controllers/singup/signup.spec.ts
+++ src/presentation/controllers/singup/signup.spec.ts
@@ -220,4 +220,21 @@ describe('SignUp Controller', () => {
       password: 'any_password',
     });
   });
+
+  test('Should be return status 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toHaveProperty('id');
+  });
 });
